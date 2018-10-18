7d3d63f1b1f764a2da5ba92d33e286f865f57cdd
Jonathan Viray <jfviray7@gmail.com>
2018-10-18 17:22:26 -0400

    Initialize repo

---
+++ contracts/Inbox.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+  string public message;
+
+  function Inbox(string _initialMessage) public {
+    message = _initialMessage;
+  }
+
+  function setMessage(string _newMessage) public {
+    message = _newMessage;
+  }
+}
